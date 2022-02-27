d11a4eb806b881334b225b04381beeb52a0edc08
Marek Pamula <Marek.Pamula@orange.com>
2022-02-27 19:06:31 +0100

    updated

--- 02/koszt_wyprawy.py
+++ 02/koszt_wyprawy.py
@@ -1,6 +1,6 @@
 """
 Oblicz koszt wyprawy znając dystans, spalanie na 100km i cenę litra benzyny.
-Załóżmy, że spalanie na 100km wynosi 6.4 l, trasa to 120km, litr benzyny kosztuje 5.04 zł.
+Załóżmy, że spalanie na 100km wynosi 6.4l, trasa to 120km, litr benzyny kosztuje 5.04 zł.
 """
 
 fuel_consumption = 6.4
