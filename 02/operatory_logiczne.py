2a53624f920c40260a6e54c0da08331db04afe99
Marek Pamula <Marek.Pamula@orange.com>
2022-02-27 20:02:16 +0100

    ćwiczenia z operatorami logicznymi

---
+++ 02/operatory_logiczne.py
@@ -0,0 +1,14 @@
+print('Czy 23 + 3 będzie się równać 15 + 12 ?: -> ', 23+3==15+12)
+
+print('Czy dzielenie 29 przez 7 bez reszty wynosi 5? -> ', 29//7 == 5)
+print('Czy 27 podzielone przez 8 daje resztę 3? -> ', 27%8 == 3)
+
+# Wyświetl True, jeżeli liczba jest parzysta
+num = int(input('Podaj liczbę całkowitą: '))
+print(f'Czy liczba {num} jest parzysta?: -> ', num%2 == 0)
+
+print('Czy 43 - 13 będzie się równać 11 + 12? -> ', 43-13==11+12)
+
+print('Czy dzielenie 129 przez 17 bez reszty wynosi 3? -> ', 129//17 ==3)
+
+print('Czy 247 podzielone przez 5 daje resztę 2? -> ', 247%5 ==2)
