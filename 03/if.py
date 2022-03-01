847bc61f2967221cf44a6e005e58d14024186cf0
Marek Pamula <Marek.Pamula@orange.com>
2022-03-01 09:04:02 +0100

    ćwiczenia z wykorzystaniem IF

---
+++ 03/if.py
@@ -0,0 +1,18 @@
+dish = "gzik"
+if dish == "pyzy":
+    reg = 'Wielkopolska'
+print(reg)
+
+
+
+season = input('Podaj porę roku: ')
+if season == 'wiosna':
+    print('zasadź rośliny')
+elif season == 'lato':
+    print('podlewaj ogród')
+elif season == 'jesien':
+    print('zbierz owoce')
+elif season == 'zima':
+    print('brrr za zimno!')
+else:
+    print('nie ma takiej pory roku')
