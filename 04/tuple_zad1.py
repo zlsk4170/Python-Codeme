a0e2d45afe1d08f4f3e6bda4b287042a377199dd
Marek Pamula <Marek.Pamula@orange.com>
2022-03-08 18:38:18 +0100

    tuple - zadanie1

---
+++ 04/tuple_zad1.py
@@ -0,0 +1,9 @@
+# Stwórz krotkę zawierającą dane twojego pupila (rodzaj zwierzecia, rasa, jak się wabi).
+# Następnie rozpakuj tę krotkę na pojedyńcze zmienne.
+# Wykorzystaj zmienne do wyświetlenia f-stringa, tak by mogło powstać zdanie
+# np. “Mój pies, rasy border collie wabi się Dyzio”.
+
+
+dog =('pies','husky','Nazar' )
+(animal, race, name) = dog
+print (f'Moj {animal} rasy {race} wabi sie {name}')
