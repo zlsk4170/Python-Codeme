9cd46be7bb3694aa951f7be99a626e187119678f
Marek Pamula <Marek.Pamula@orange.com>
2022-03-09 09:29:50 +0100

    dictionary - zadanie1

--- 04/dic_zad1.py
+++ 04/dic_zad1.py
@@ -1,5 +1,5 @@
 # Utwórz listę lists_to_dict zawierającą listy 2 elementowe.
-# Przekształć ją w słownik dict_from_list.
+# Przekształć ją w słownik dict_from_list .
 
 lists_to_dict = [('a',1),('b',2),('c',6)]
 dict_from_list = dict(lists_to_dict)
