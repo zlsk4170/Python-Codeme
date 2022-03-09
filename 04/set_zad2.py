42be34ba9dfe6c27646b8ce690aebce8d58b1c8c
Marek Pamula <Marek.Pamula@orange.com>
2022-03-09 08:54:04 +0100

    set - zadanie2

---
+++ 04/set_zad2.py
@@ -0,0 +1,46 @@
+# Utwórz listę L_test = [1, 2, 3, 4], krotkę T_test = (1, 2, 3, 4)
+# oraz set S_test = {1, 2, 3, 4}.
+# Jakie metody dostępne dla typu list nie będą działać dla typów tuple i set?
+
+# metody listy:
+
+L_test = [1,2,3,4]
+
+L_test.append()
+L_test.clear()
+L_test.copy()
+L_test.count()
+L_test.extend()
+L_test.index()
+L_test.insert()
+L_test.pop()
+L_test.remove()
+L_test.reverse()
+L_test.sort()
+
+# metody tuple
+T_test = (1,2,3,4)
+
+T_test.count()
+T_test.index()
+
+# metody set
+S_test = {1,2,3,4}
+
+S_test.add()
+S_test.clear()
+S_test.copy()
+S_test.difference()
+S_test.difference_update()
+S_test.discard()
+S_test.intersection()
+S_test.intersection_update()
+S_test.isdisjoint()
+S_test.issubset()
+S_test.issuperset()
+S_test.pop()
+S_test.remove()
+S_test.symmetric_difference()
+S_test.symmetric_difference_update()
+S_test.union()
+S_test.update()
