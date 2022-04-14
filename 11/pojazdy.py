8392bdd9769c1f98484688b602b8ec0bc2f7eca9
Marek Pamula <Marek.Pamula@orange.com>
2022-04-14 12:44:19 +0200

    pojazdy

---
+++ 11/pojazdy.py
@@ -0,0 +1,14 @@
+class Vehicles():
+    pass
+    # cecha: mają koła
+    # metoda: poruszają się()
+
+class Bikes(Vehicles):
+    pass
+    #cecha1: mają 2 kołą
+    #cecha2: mają inną kierownicę
+
+class Cars(Vehicles):
+    #checha1: mają silnik
+    #checha2: mają 4 koła
+    #metoda: uruchom silnik
